19dfc233ffc5beedc7a01f20e7b0a19936881c95
acorteztouzett <mcorteztouzett@gmail.com>
2023-06-24 20:56:42 -0500

    feat: login register created

--- Views/VentanaMain.h
+++ Views/VentanaMain.h
@@ -2,6 +2,9 @@
 #pragma once
 #include<iostream>
 #include "ventanaEstudianteInicio.h"
+#include <fstream>
+#include <string>
+#include <sstream>
 using namespace std;
 
 
@@ -10,7 +13,74 @@ public:
     VentanaMain(){
 
     }
+    bool iniciarSesion(const std::string& nombreUsuario, const std::string& contraseña) {
+        std::ifstream archivo("../db/users.txt");  // Abre el archivo en modo lectura
 
+        if (!archivo) {
+            std::cerr << "No se pudo abrir el archivo." << std::endl;
+            return false;
+        }
+
+        std::string linea;
+        while (std::getline(archivo, linea)) {  // Lee una línea del archivo
+            // Extrae el nombre de usuario y la contraseña de la línea
+            std::istringstream iss(linea);
+            std::string usuario;
+            std::string clave;
+            iss >> usuario >> clave;
+
+            // Compara el nombre de usuario y la contraseña con los datos proporcionados
+            if (usuario == nombreUsuario && clave == contraseña) {
+                archivo.close();  // Cierra el archivo
+                return true;  // Inicio de sesión exitoso
+            }
+        }
+
+        archivo.close();  // Cierra el archivo
+
+        return false;  // Inicio de sesión fallido
+    }
+
+    bool iniciarSesionUsuario() {
+        std::string nombreUsuario;
+        std::string contraseña;
+
+        std::cout << "Ingrese su nombre de usuario: ";
+        std::cin >> nombreUsuario;
+
+        std::cout << "Ingrese su contraseña: ";
+        std::cin >> contraseña;
+
+        if (iniciarSesion(nombreUsuario, contraseña)) {
+            std::cout << "Inicio de sesión exitoso." << std::endl;
+            return true;
+        }
+        std::cout << "Inicio de sesión fallido. Verifique sus credenciales." << std::endl;
+        return false;
+    }
+    void registrarUsuario() {
+        std::string nombreUsuario;
+        std::string contraseña;
+
+        std::cout << "Ingrese un nombre de usuario: ";
+        std::cin >> nombreUsuario;
+
+        std::cout << "Ingrese una contraseña: ";
+        std::cin >> contraseña;
+
+        std::ofstream archivo("../db/users.txt", std::ios::app); // Abre el archivo en modo adjuntar
+
+        if (!archivo) {
+            std::cerr << "No se pudo abrir el archivo." << std::endl;
+            return;
+        }
+
+        // Registrar al nuevo usuario
+        archivo << nombreUsuario << " " << contraseña << std::endl;
+        archivo.close();
+
+        std::cout << "Registro exitoso. El usuario ha sido registrado correctamente." << std::endl;
+    }
 
     void iniciar()
     {
@@ -34,13 +104,16 @@ public:
             case 1:{
                 // Aquí puedes agregar la lógica para el inicio de sesión
                 cout << "Has seleccionado 'Iniciar sesión'." << endl;
+                if (!iniciarSesionUsuario()) {
+                    break;
+                }
                 VentanaEstudianteInicio ventanaEstudiante;
                 ventanaEstudiante.Inicio();
-                break;
             }
             case 2:
                 // Aquí puedes agregar la lógica para el registro de usuarios
                 cout << "Has seleccionado 'Registrarse'." << endl;
+                registrarUsuario();
                 break;
             case 3:
                 cout << "Gracias por utilizar nuestro sistema. ¡Hasta luego!" << endl;
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/user/CLionProjects/ProyectoFinalEDA
+# Install script for directory: C:/Users/mcort/OneDrive/Escritorio/ProyectoFinalEDA
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/user/CLionProjects/ProyectoFinalEDA/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/mcort/OneDrive/Escritorio/ProyectoFinalEDA/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
