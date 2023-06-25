6c41c8389df63f6bb8814cfa8d5a2e68f0755451
andre <andresdaniel.go2020@gmail.com>
2023-06-24 19:48:28 -0500

    Agregando

---
+++ cmake-build-debug/CMakeFiles/ProyectoFinalEDA.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/andre/OneDrive/Documentos/ProyectoFinalEDA/Views/VentanaEstudianteInicio.cpp" "CMakeFiles/ProyectoFinalEDA.dir/Views/VentanaEstudianteInicio.cpp.obj" "gcc" "CMakeFiles/ProyectoFinalEDA.dir/Views/VentanaEstudianteInicio.cpp.obj.d"
+  "C:/Users/andre/OneDrive/Documentos/ProyectoFinalEDA/Views/VentanaMain.cpp" "CMakeFiles/ProyectoFinalEDA.dir/Views/VentanaMain.cpp.obj" "gcc" "CMakeFiles/ProyectoFinalEDA.dir/Views/VentanaMain.cpp.obj.d"
+  "C:/Users/andre/OneDrive/Documentos/ProyectoFinalEDA/main.cpp" "CMakeFiles/ProyectoFinalEDA.dir/main.cpp.obj" "gcc" "CMakeFiles/ProyectoFinalEDA.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
